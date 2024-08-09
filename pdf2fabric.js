542466d3040d6dd1279687b8cb8ce6dc1f06c34c
Nevermore <15972699417@163.com>
2024-08-09 13:56:03 +0800

    更新解析

--- pdf2fabric.js
+++ pdf2fabric.js
@@ -2,14 +2,15 @@
 import fs from "fs";
 import path from 'path';
 import pdfjs from "pdfjs-dist";
+import { SVGGraphics } from "./src/display/svg.js";
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(path.dirname(__filename));
-
+const { OPS, Util, ImageKind, AnnotationMode } = pdfjs
 
 const main = async (pdfPath) => {
-  const { OPS, Util, ImageKind, AnnotationMode } = pdfjs
+  
 
   const loadingTask = pdfjs.getDocument({
     url: pdfPath,
@@ -24,11 +25,12 @@ const main = async (pdfPath) => {
     const objs = page.objs
     const viewport = page.getViewport({ scale: 1.0 })
     const operatorList = await page.getOperatorList()
-    const svgGfx = new pdfjsLib.SVGGraphics(commonObjs, objs);
+    const svgGfx = new SVGGraphics(commonObjs, objs);
     // svgGfx.getSVG(operatorList, viewport);
     const dependencies = await svgGfx.loadDependencies(operatorList)
     const covertOperatorList = svgGfx.convertOpList(operatorList)
-    
+    svgGfx.executeOpTree(covertOperatorList)
+    // console.log(covertOperatorList)
     
     //   this.transformMatrix = _util.IDENTITY_MATRIX;
     //   this.executeOpTree(this.convertOpList(operatorList));
